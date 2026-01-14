fe8ba50f6d01cdaba9f8cc1229ce3a72fa44cbbb
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2026-01-14 23:57:31 +0800

    chore: add starter files

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Personal best:</p>
+
+      <p>WPM:</p>
+      <p>Accuracy:</p>
+      <p>Time:</p>
+
+      <p>Difficulty</p>
+      <p>Easy</p>
+      <p>Medium</p>
+      <p>Hard</p>
+
+      <p>Mode:</p>
+      <p>Timed (60s)</p>
+      <p>Passage</p>
+
+      <p>Start Typing Test</p>
+      <p>Or click the text and start typing</p>
+
+      <p>Test Complete!</p>
+      <p>Solid run. Keep pushing to beat your high score.</p>
+
+      <p>WPM:</p>
+      <p>Accuracy:</p>
+      <p>Characters:</p>
+
+      <p>Go Again</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
